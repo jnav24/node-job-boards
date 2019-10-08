3e2151e2f608cbe658ae1bd0a87f6baee27819c8
Justin Navarro <info@justinnavarro.net>
2019-10-08 18:50:04 -0400

    initial setup of mongoose

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const mongoose = require('mongoose');
 const port = 9000;
 
 const app = express();
+
+mongoose.connect('mongodb://root:root@localhost:27017/jobBoards');
 
 app.use(
     cors(),
