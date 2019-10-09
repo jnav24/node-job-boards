82a4be8d1e6a81de69c097d91a913345fe813052
Justin Navarro <info@justinnavarro.net>
2019-10-09 10:43:26 -0400

    added bcrypt configuration update login endpoint

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
@@ -6,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 const mongoose = require('mongoose');
 const port = 9000;
+const saltRounds = 10;
 
 const app = express();
 
@@ -14,10 +16,11 @@ mongoose.connect('mongodb://job:boards@localhost:27017/jobBoards', {
     useUnifiedTopology: true,
 });
 
-const Postings = require('./models/postings');
+const Users = require('./models/users');
 
 app.use(
     cors(),
+    bodyParser.urlencoded({ extended: true }),
     bodyParser.json(),
     expressJwt({
         secret: jwtSecret,
@@ -25,13 +28,18 @@ app.use(
     })
 );
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const {email, password} = req.body;
-    const user = {
-        password: '',
-    };
 
-    if (!user && user.password !== password) {
+    if (!email || !password) {
+        res.sendStatus(401);
+        return;
+    }
+
+    const hashPwd = bcrypt.hashSync(password, saltRounds);
+    const user = await Users.findOne({ email }).exec();
+
+    if (!user || user.password !== hashPwd) {
         res.sendStatus(401);
         return;
     }
