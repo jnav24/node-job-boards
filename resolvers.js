130b52ad8110d01c0b6f2868e9d7722870022b2c
Justin Navarro <info@justinnavarro.net>
2020-07-20 23:37:52 -0400

    grab company with jobs details

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,15 @@
+const Companies = require('./models/companies');
 const Jobs = require('./models/postings');
 
 const Query = {
     jobs: async () => await Jobs.find().exec(),
 };
 
+const Job = {
+    company: async job => await Companies.findById(job.company).exec(),
+};
+
 module.exports = {
     Query,
+    Job,
 };
